af35d585165fe96da1619b0c3c8962529cbae28c
Drin <109246823+DrinMarevci@users.noreply.github.com>
2023-02-27 10:06:30 +0100

    Almost there

--- src/components/Invoice.js
+++ src/components/Invoice.js
@@ -1,44 +1,62 @@
 import React from "react";
 
 function Invoice({ items }) {
-    const totalPrice = items.reduce(
-        (total, { price_per_unit, quantity }) => total + price_per_unit * quantity,
-        0
-    );
+  const invoices = [];
+  let currentInvoice = { items: [], totalPrice: 0 };
 
-    return (
-        <div className="invoice">
-            <h2>Invoice</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Item Name</th>
-                        <th>Price $</th>
-                        <th>VAT %</th>
-                        <th>Quantity</th>
-                        <th>Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.map((item) => (
-                        <tr key={item.id}>
-                            <td>{item.name}</td>
-                            <td>{item.price_per_unit} $</td>
-                            <td>{item.vat} %</td>
-                            <td>{item.quantity}</td>
-                            <td>{item.price_per_unit * item.quantity}</td>
-                        </tr>
-                    ))}
-                </tbody>
-                <tfoot>
-                    <tr>
-                        <td colSpan="4">Total</td>
-                        <td>{totalPrice}</td>
-                    </tr>
-                </tfoot>
-            </table>
+  items.forEach((item) => {
+    const price = item.price_per_unit * item.quantity * (1 + item.vat / 100);
+    if (currentInvoice.totalPrice + price > 500) {
+      invoices.push(currentInvoice);
+      currentInvoice = { items: [], totalPrice: 0 };
+    }
+    currentInvoice.items.push(item);
+    currentInvoice.totalPrice += price;
+  });
+
+  if (currentInvoice.items.length > 0) {
+    invoices.push(currentInvoice);
+  }
+
+  return (
+    <div>
+      {invoices.map((invoice, index) => (
+        <div className="invoice" key={index}>
+          <h2>Invoice {index + 1}</h2>
+          <table>
+            <thead>
+              <tr>
+                <th>Item Name</th>
+                <th>Price $</th>
+                <th>VAT %</th>
+                <th>Quantity</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {invoice.items.map((item) => (
+                <tr key={item.id}>
+                  <td>{item.name}</td>
+                  <td>{item.price_per_unit} $</td>
+                  <td>{item.vat} %</td>
+                  <td>{item.quantity}</td>
+                  <td>
+                    {(item.price_per_unit * item.quantity * (1 + item.vat / 100)).toFixed(2)} $
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+              <tr>
+                <td colSpan="4">Total</td>
+                <td>{invoice.totalPrice.toFixed(2)} $</td>
+              </tr>
+            </tfoot>
+          </table>
         </div>
-    );
+      ))}
+    </div>
+  );
 }
 
 export default Invoice;
